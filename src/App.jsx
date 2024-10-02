dbe678aa350ee596effdb6f9ae35548d4c98a060
Portella10 <144740763+Portella10@users.noreply.github.com>
2024-10-02 12:50:21 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,31 @@
-import React, { useEffect, useState } from 'react';
-import './App.css'; // Certifique-se de que o CSS está importado corretamente
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import SeriesDetails from './pages/SeriesDetails';
+import CastMember from './pages/CastMember';
+import ProductionCompany from './pages/ProductionCompany';
+import About from './pages/About';
 
 function App() {
-  const [series, setSeries] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('https://api.tvmaze.com/shows');
-        const data = await response.json();
-        console.log('Dados recebidos:', data); // Para depuração
-        setSeries(data);
-        setLoading(false);
-      } catch (error) {
-        console.error('Erro ao buscar dados:', error);
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  if (loading) {
-    return <div className="loading">Loading...</div>;
-  }
-
   return (
-    <div className="app">
-      <header className="header">
-        <h1>Catalógo de Séries</h1>
-        <h2>Séries Populares</h2>
-      </header>
-      <ul className="series-list">
-        {series.slice(0, 10).map((serie) => (
-          <li key={serie.id} className="series-item">
-            <img src={serie.image?.medium} alt={serie.name} className="series-image" />
-            <div className="series-info">
-              <h3 className="series-title">{serie.name}</h3>
-              <p className="series-summary">{serie.summary.replace(/(<([^>]+)>)/gi, "")}</p>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <Router>
+      <div className="app">
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">Sobre</Link></li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/series/:id" component={SeriesDetails} />
+          <Route path="/cast-member/:id" component={CastMember} />
+          <Route path="/production-company/:id" component={ProductionCompany} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
